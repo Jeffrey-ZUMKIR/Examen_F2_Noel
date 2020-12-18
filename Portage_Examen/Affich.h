ce819a5afaad80b64ff615507bb0693dd22895f6
Osef1712 <j.zumkir@ludus-academie.com>
2020-12-18 11:23:41 +0100

    Modif_GH
    Delete programme C dans la branche

--- Portage_Examen/Affich.h
+++
@@ -1,8 +0,0 @@
-#ifndef AFFICH_H_INCLUDED
-#define AFFICH_H_INCLUDED
-#include "defineStruct.h"
-
-extern void AffichageInit(char tab[HEIGHTAB][WIDTHTAB]);
-extern void AffichageTrace(int tabTrace[HEIGHTAB][WIDTHTAB],char tabAff[HEIGHTAB][WIDTHTAB]);
-
-#endif // AFFICH_H_INCLUDED
